4cd9ff6e2f42e1af1fd4c1917f96d1b5abd5b4e8
Ian caironi <iancaironi8@gmail.com>
2023-12-08 00:09:29 -0300

    frontend

---
+++ routes/api.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+var novedadesModel = require('./../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
+
+
+
+router.get('/novedades', async function(req, res, next) {
+    let novedades = await novedadesModel.getNovedades();
+   
+
+   
+    novedades = novedades.map(novedad => {
+        if (novedad.img_id) {
+            const imagen = cloudinary.url(novedad.img_id, {
+                width: 960,
+                height: 200,
+                crop: 'fill'
+            });
+            return {
+                ...novedad,
+                imagen
+            }
+        } else {
+            return {
+                ...novedad,
+                imagen: ''
+            }
+        }
+    });
+
+
+    res.json(novedades);
+});
+
+
+
+// 
+
+router.post('/contacto', async(req, res) =>{
+    const mail = {
+        to:'iancaironi8@gmail.com',
+        subject: 'Contacto',
+        html: `${req.body.nombre} se contactó a traves de la web y quiere más información a este correo ${req.body.email}
+        <br> Además, hizo el siguiente comentario: ${req.body.mensaje} <br> Su tel es: ${req.body.telefono}`
+    }
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await transport.sendMail(mail);
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado'
+    })
+});
+
+
+
+module.exports = router;
